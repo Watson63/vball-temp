4e8ab6e1c3c6c93a9308ae5ef688e33db4b9705d
Watson63 <149442371+Watson63@users.noreply.github.com>
2025-01-28 21:45:26 -0700

    Add files via upload

---
+++ masterSchedule.js
@@ -0,0 +1,6 @@
+const schedule = {
+    '2025-01-28': ['Team A vs Team B', 'Team B vs Team D'],
+    '2025-01-29': ['Team C vs Team D'],
+    '2025-01-30': ['Team K vs Team L'],
+    '2025-02-02': ['Team M vs Team N']
+  };
