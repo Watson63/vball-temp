bba79b066f5a9f366551216586cca147a6bace30
Watson63 <149442371+Watson63@users.noreply.github.com>
2025-02-02 13:26:58 -0700

    Update masterSchedule.js

--- masterSchedule.js
+++ masterSchedule.js
@@ -2,7 +2,7 @@ const schedule = {
     '2025-01-28': ['Team A vs Team B', 'Team B vs Team D'],
     '2025-01-29': ['Team C vs Team E', 'Team F vs Team G'],
     '2025-01-30': ['Team H vs Team I', 'Team J vs Team K','Team L vs Team M', 'Team N vs Team O'],
-    '2025-02-03': ['Team L vs Team Q']
+    '2025-02-3': ['Team L vs Team Q']
 
     
   };
